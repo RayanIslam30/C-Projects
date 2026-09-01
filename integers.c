c39bedc8dc54af9259dfbfad71bf6338c1bd6ade
RayanIslam30 <rayislam2007@gmail.com>
2026-08-31 22:10:50 -0400

    changes

--- integers.c
+++ integers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-//Code that takes in a single digit integer from user and outputs how many times that digit appears in a different integer.
+//Code that takes in a number and a digit from user and outputs how many times that digit appears in the number.
 int main()
 {
     int num, digit, count = 0;
     printf("Enter a number: ");
     fflush(stdout);
     scanf("%d", &num);
+    printf("Enter a digit to count: ");
     return 0;
 }
