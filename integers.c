1e28d424d1363322a15cae1b0218b5b9da8e3df1
RayanIslam30 <rayislam2007@gmail.com>
2026-08-31 21:59:14 -0400

    getting user input and modifying it

---
+++ integers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Code that takes in a single digit integer from user and outputs how many times that digit appears in a different integer.
+int main()
+{
+    int num, digit, count = 0;
+    printf("Enter a number: ");
+    fflush(stdout);
+    scanf("%d", &num);
+    return 0;
+}
