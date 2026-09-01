0aed6f99b2b04cb07baf083b919a2f2722da4396
RayanIslam30 <rayislam2007@gmail.com>
2026-08-31 22:34:15 -0400

    fixed output

--- integers.c
+++ integers.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-//Code that takes in a number and a digit from user and outputs how many times that digit appears in the number.
+
 int main()
 {
-    int num, digit;
-    int count = 0; //initialize count to 0
+    int num, digit, count;
+    count=0;
     printf("Enter a number: ");
     fflush(stdout);
     scanf("%d", &num);
     printf("Enter a digit to count: ");
     fflush(stdout);
     scanf("%d", &digit);
-         //loop through each digit of number, see if it matches the digit we are looking for, and increment count if it does
-    while (num != 0) {
-        if (num % 10 == digit) {
+    //loop through each digit of number, see if it matches the digit we are looking for, and increment count if it does
+    int tempNum = num;
+    while (tempNum != 0) {
+        if (tempNum % 10 == digit) {
             count++;
         }
-        num /= 10; //remove last digit from number, iterate
+        tempNum /= 10; //remove last digit from number, iterate
     }
-    printf("The digit %d appears %d times in %d\n", digit, count, num); //print out how many times the digit appears in the number
+    printf("The digit %u appears %u times in %u.\n", digit, count, num); //print out how many times the digit appears in the number
     return 0;
 }
