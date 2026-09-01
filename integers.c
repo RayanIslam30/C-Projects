79e1bf4d21dc944e70d5c40ca3115a1120ddcc94
RayanIslam30 <rayislam2007@gmail.com>
2026-08-31 22:37:51 -0400

    added edge case detection

--- integers.c
+++ integers.c
@@ -12,12 +12,14 @@ int main()
     scanf("%d", &digit);
     //loop through each digit of number, see if it matches the digit we are looking for, and increment count if it does
     int tempNum = num;
+    if ((tempNum == 0) && (tempNum==digit)); //if the number is 0 and the digit is 0, increment count, catch this edge case
+        count++;
     while (tempNum != 0) {
         if (tempNum % 10 == digit) {
             count++;
         }
         tempNum /= 10; //remove last digit from number, iterate
     }
-    printf("The digit %u appears %u times in %u.\n", digit, count, num); //print out how many times the digit appears in the number
+    printf("The digit %d appears %d times in %d.\n", digit, count, num); //print out how many times the digit appears in the number
     return 0;
 }
