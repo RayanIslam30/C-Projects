9b30a63281d8229d88b0d754c2f0e8dcb30dbb98
RayanIslam30 <rayislam2007@gmail.com>
2026-09-21 15:20:37 -0400

    computes driving cost based on miles driven and gas cost per mile

---
+++ driving_cost.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//code to compute the driving cost based on miles per gallon, dollars per gallon, and miles driven
+
+double DrivingCost(double milesPerGallon, double dollarsPerGallon, double milesDriven){
+   return (milesDriven / milesPerGallon) * dollarsPerGallon; // compute the value of driving cost 
+}
+int main(void) {
+    /ask user for input
+    double milesPerGallon, dollarsPerGallon, milesDriven;
+    printf("Enter miles per gallon: ");
+    scanf("%lf", &milesPerGallon);
+    printf("Enter dollars per gallon: ");
+    scanf("%lf", &dollarsPerGallon);
+    printf("Enter miles driven: ");
+    scanf("%lf", &milesDriven);
+    // compute and display the driving cost
+    double cost = DrivingCost(milesPerGallon, dollarsPerGallon, milesDriven);
+    printf("The driving cost is: $%.2f\n", cost);
+
+   return 0;
+}
